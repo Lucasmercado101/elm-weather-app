ef999478a9135882d32a493a6668f7bb56a86b74
Lucasmercado101 <lucasmercado101@gmail.com>
2023-02-22 15:47:29 -0300

    added gulp and gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+const { src, dest, series, parallel } = require("gulp");
+const elm = require("gulp-elm");
+const uglify = require("gulp-uglify");
+const htmlmin = require("gulp-htmlmin");
+const cleanCss = require("gulp-clean-css");
+
+const elmToJs = () =>
+  src("src/Main.elm")
+    .pipe(
+      elm.bundle("elm.js", {
+        optimize: true
+      })
+    )
+    .pipe(uglify())
+    .pipe(dest("dist"));
+
+const html = () =>
+  src("public/index.html")
+    .pipe(
+      htmlmin({
+        collapseWhitespace: true,
+        minifyCSS: true,
+        removeComments: true
+      })
+    )
+    .pipe(dest("dist"));
+
+const css = () => src("public/styles.css").pipe(cleanCss()).pipe(dest("dist"));
+
+const copyAssets = () => src("public/assets/*").pipe(dest("dist/assets"));
+
+const uglifyJs = () => src("public/**/*.js").pipe(uglify()).pipe(dest("dist"));
+
+exports.default = parallel(elmToJs, html, css, copyAssets, uglifyJs);
